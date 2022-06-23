756cb1c4e9b3d1c02bfe4f974ee1f0b1dde1f9c9
thanhphongcpt <73149529+thanhphongcpt@users.noreply.github.com>
2022-06-23 12:14:55 +0700

    bai 1

--- Class_2.rs
+++ Class_2.rs
@@ -5,23 +5,21 @@
 
 //Exercise 1
 // Mục đích: giải quyết vấn đề ownership and borrowing không dùng clone()
-// fn main() {
-    
-//     let x = change_value(10,20);
-// }
+ fn main() {
+       let mut y: u32 = 0;
+     let x = change_value(10,&mut y);
+ }
 
+ fn change_value(input:u32, output: &mut u32) -> u32{
+     if input ==1 {
+         *output =3;
+     }
+     else {
+         *output = 4;
+     }
 
-
-// fn change_value(input:u32, output: &mut u32) -> u32{
-//     if input ==1 {
-//         *output =3;
-//     }
-//     else {
-//         *output = 4;
-//     }
-
-//     *output
-// }
+     *output
+ }
 
 
 //Exercise 2
