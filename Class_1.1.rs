4987b9c4a0dca3606ecd1cb651efc488f0a903f6
thanhphongcpt <73149529+thanhphongcpt@users.noreply.github.com>
2022-06-23 10:49:03 +0700

    bai 2

--- Class_1.1.rs
+++ Class_1.1.rs
@@ -1,7 +1,7 @@
 //Bài tập 1: Cho 2 mảng, kiểm tra mảng này có phải là mảng con của mảng kia không ? (yêu cầu đúng thứ tự của các phần tử)
 //Ví dụ : let org_arr = [1, 2,3,5,6,8, 10, 11];
 //            let sub_arr = [6,8,10];
-
+use std::{array, io};
 
 fn main() {
     let  arr_big: [i32;4] = [3,2,4,9 ];
@@ -29,4 +29,42 @@ fn main() {
         println!("false");
     }
    
-}
+    /*------------------------------------------------------------------------ */
+    //bai 2
+    /*------------------------------------------------------------------------ */
+//input tu ban phim
+    let og_ =
+           "This is a regular paragraph with the default style of Normal. 
+            This is a regular paragraph with the default style of Normal. 
+            This is a regular paragraph with the default style of Normal.
+            This is a regular paragraph with the default style of Normal. 
+            This is a regular paragraph with the default style of Normal.";
+
+    let mut repeat = 0;
+    let mut input_char = String::new();
+    println!("Nhap tu ban phim: ");
+    io::stdin().read_line(&mut input_char).unwrap();
+    input_char.pop();
+    let mut _t;
+    let mut new_str;
+
+//kiem tra ki tu
+
+    if let Some(p_index) = og_.find(&input_char)
+    { 
+        new_str = og_.split_at( p_index + input_char.len());
+        _t = new_str.1;
+        repeat +=1;
+        while let Some(c) = _t.find(&input_char){
+            new_str = _t.split_at( c + input_char.len()); 
+            _t = new_str.1;
+            repeat +=1;
+        }
+        println!("p_index: {}",repeat);
+
+   
+    }
+    else {
+        println!("None");
+    }
+}fn main
